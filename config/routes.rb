005ee7438cce6863ed062391db236d71330cfea4
Thierry Laporte <laportethierry@yahoo.fr>
2020-11-12 16:52:07 +0100

    cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to:"cocktails#index"
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create, :destroy]
+  end
 end
+
+  # get "cocktails", to: "cocktails#index"
+
+  # get "cocktails/new", to: "cocktails#new",  as: :new_cocktail
+  # post "cocktails", to: "cocktails#create"
+  
+  # get "cocktails/:id", to: "cocktails#show", as: :cocktail
+
+  # get "cocktails/:id/edit", to: "cocktails#edit", as: :edit_cocktail
+  # patch "cocktails/:id", to: "cocktails#update"
+
+  # delete "cocktails/:id", to: "cocktails#destroy"
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list" 
+parse = JSON.parse(open(url).read) 
+parse["drinks"].each do |ingredient| 
+  Ingredient.create(name: ingredient["strIngredient1"]) 
+end
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+Cocktail.create(name: "White Russian")
+Cocktail.create(name: "Sex on the Beach")
+Cocktail.create(name: "Mojito")
+
+Dose.create(description: "4.5 cl")
+Dose.create(description: "9 cl")
+Dose.create(description: "1.33 oz")
+Dose.create(description: "2 cl")
